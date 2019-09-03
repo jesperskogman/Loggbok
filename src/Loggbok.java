033be39196e14b854d86a8c667972b98ee22fa33
jesperskogman <jesper.skogman@elev.ga.ntig.se>
2019-09-03 09:27:36 +0200

    Början till en meny

---
+++ src/Loggbok.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Loggbok {
+    public static void main(String[] args) {
+        Scanner tgb = new Scanner(System.in);
+        printMenu();
+        int input = tgb.nextInt();
+        ArrayList<Strings> posts
+
+                
+
+
+
+
+
+
+
+
+    }
+
+    private static void printMenu() {
+    }
+}
